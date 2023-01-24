a4468f087cfe45ce528f12f11ad8c8b10ddc0d16
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-01-24 20:48:05 +0000

    Completed file 2

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -5,11 +5,52 @@
 # Booleans
 
 # Boolean Expressions
+puts 3 > 2 
+puts 3 < 2 
+puts 3 == 2 
+puts 3 != 2 
 
 # If Conditional Logic
+if 3 > 2
+    puts "awesome"
+    puts "math works!"
+end 
+
+if 3 < 2 
+    puts "what????"
+end
 
 # If/Else Conditional Logic
+if 3 > 2
+    puts "awesome"
+    puts "math works!"
+else 
+    puts "what????"
+end
+
+password = "Tacos"
+user_entered_password = "password1"
+if password == user_entered_password
+    puts "You're in"
+else 
+    puts "go away"
+end
 
 # Elsif Conditional Logic
+home_team_score = 101
+away_team_score = 101
+if home_team_score > away_team_score
+    puts "winner!"
+elsif home_team_score < away_team_score
+    puts "loser"
+else 
+    puts "tied"
+end
 
 # Combining Expressions
+temp = 68 
+if temp < 80 && temp > 60
+    puts "it's beautiful"
+else 
+    puts "brrr"
+end
