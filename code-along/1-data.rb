2d5d8c1d95c1f3fef8cb24c61c5f9c9515b3c962
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-01-24 20:20:03 +0000

    Completed first file

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -9,13 +9,37 @@ puts 5
 puts 2
 
 # Perform simple math with numbers
+puts 5 + 2
+puts 5 * 2 
+puts 5 / 2
+puts 5 % 2
+
+puts 5.0 / 2.0
+puts 5.0 / 2
+
+puts 5 * (2 + 1)
 
 # Strings
+puts 'Hello, world'
 
 # Combine strings together
+puts "Tacos are " + "delish"
+
+puts "Tacos" * 3
+puts "Tacos:" + 3.to_s
 
 # Variables
+food = "Tacos"
+quantity = 3 
+puts food * quantity
 
 # Combine strings and variables
+first_name = "Group"
+puts "Hello, " + first_name + "!"
+
+greeting = "Hello, #{first_name}!"
+puts greeting 
+
+puts "#{food}: #{quantity}"
 
 # String manipulation
